4245fd939d0d9ade6229f911caf2d028f4c84bf7
Farshad <FarshadJanmohammadi@gmail.com>
2022-04-16 11:35:23 +0430

    end of single page

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,13 +1,5 @@
 import React from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import "./chart.scss";
 
 const data = [
@@ -19,11 +11,11 @@ const data = [
   { name: "June", Total: 1700 },
 ];
 
-const Chart = () => {
+const Chart = ({ aspect, title }) => {
   return (
     <div className="chart">
-      <div className="title">Last 6 Months (Revenue) </div>
-      <ResponsiveContainer width="100%" aspect={2 / 1}>
+      <div className="title">title</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart
           width={730}
           height={250}
